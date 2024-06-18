5aa2da352e6c82477a377a64265a07cb2bb28743
adrien <adrien.no@proton.me>
2024-06-18 15:33:37 +0200

    labelled/unlabelled working clean

--- bin/main.ml
+++ bin/main.ml
@@ -5,11 +5,11 @@ let draw_diagram () =
   let c = ref 0 in
   let continue = ref true in
   while !continue do
-      try
-        Lib.Draw.pin_dot_and_typo (Sys.getcwd() ^ Printf.sprintf "/img/diagram%i.dot" !c);
-        incr c
-      with Sys_error _s -> (* Printf.printf "dernier fichier+1: %s\n" s; *)
-        continue := false
+    try
+      Lib.Draw.pin_dot_and_typo (Sys.getcwd() ^ Printf.sprintf "/img/diagram%i.dot" !c);
+      incr c
+    with Sys_error _s -> (* Printf.printf "dernier fichier+1: %s\n" s; *)
+      continue := false
   done
 
 (* let _ = *)
@@ -26,13 +26,29 @@ let okada2() =
   List.iter Okada.print planar
 
 let _ =
-  let planar =
-  List.init 6 (fun k ->
+  (* let planar = *)
+  (*   List.init 6 (fun k -> *)
+  (*       let module Okada = Lib.Labelled.Make(struct let k = k end) in *)
+  (*       let planar = Okada.generate [B; P; Id] in *)
+  (*       planar) in *)
+  (* let planar_seq = List.map List.length planar in *)
+  (* Lib.Toolbox.string_of_int_list planar_seq |> Printf.printf "planar Okada: %s\n"; *)
+  (* Lib.Toolbox.string_of_int_list (List.map (fun x -> Lib.Maths.prime_decomp x |> List.length ) planar_seq) |> Printf.printf "nb_decomp: %s\n"; *)
+  (* okada2(); *)
+  draw_diagram()
+
+(* stabilisateurs *)
+let _ =
+  for k = 1 to 2 do
     let module Okada = Lib.Labelled.Make(struct let k = k end) in
     let planar = Okada.generate [B; P; Id] in
-    planar) in
-  let planar_len = List.map List.length planar in
-  Lib.Toolbox.string_of_int_list planar_len |> Printf.printf "planar Okada: %s\n";
-  Lib.Toolbox.string_of_int_list (List.map (fun x -> Lib.Maths.prime_decomp x |> List.length ) planar_len) |> Printf.printf "nb_decomp: %s\n";
-  okada2();
-  draw_diagram()
+    List.map (fun d ->
+        let rec stabilize d' acc =
+          let new_d = d @ d' in
+          if new_d = d' then (acc+1)
+          else stabilize new_d (acc+1)
+        in
+        stabilize d 0
+      ) planar
+    |> Lib.Toolbox.string_of_int_list |> Printf.printf "k = %i stabilisateurs : %s" k
+  done
---
+++ bin/unlabelled.ml
@@ -0,0 +1,3 @@
+(* let D2 = Lib.Unlabelled.Diagram(struct let k = 2 end) *)
+
+(* let _ = Printf.printf "Salut\nz" *)
